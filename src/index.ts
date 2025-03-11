ac80a1ac1ddaa14adb35d5a6aac118f53c914fad
Leticia Leal <leticialealp@hotmail.com>
2025-03-10 22:34:56 -0300

    executa testes de cadastro e listagem de alunos

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello TypeS!");
+import { AlunosRepository } from "./repositories/aluno.repository";
+
+const alunosRepository = new AlunosRepository();
+
+// alunosRepository.create({
+//   nome: "João",
+//   sobrenome: "Leal",
+//   cpf: "00011122244",
+//   email: "joao@gmail.com",
+// });
+
+// alunosRepository.listAll().then((dados) => console.log(dados));
+
+// alunosRepository.getById(3).then((dado) => console.log(dado));
